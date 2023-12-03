379a523d03b3ca4bbe8394842997a31a609ddb2e
Nehoa21 <nehoaakina21@yahoo.com>
2023-12-02 17:56:58 -0700

    edited text

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,10 +8,10 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await findAll({ 
+    const productData = await Product.findAll({ 
       include: [{
-        model: Category, Tag,
-      }],
+        model: Category, Tag
+      }]
     });
     res.status(200).json(productData);
   } catch (err) {
